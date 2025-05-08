6d45413bc11958d2ab70593a5b441a0bad02f84a
Miftahush Syaadah <miftahussyaadah12@gmail.com>
2025-05-08 21:16:11 +0800

    Menampilkan program menu array

--- menu.cpp
+++ menu.cpp
@@ -2,6 +2,10 @@
 #include <conio.h>
 using namespace std;
 
+const int MAX = 100;
+int data[MAX];
+int jumlah = 0;
+
 void info(){
   system("cls");
   cout<<"Indonesia Hebat, Poliban Juara";
@@ -9,20 +13,71 @@ getch();
 }
 
 void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
+  system("cls");
+  cout<<"Aplikasi Tampilan Menu"<<"\n";       
+  cout<<"1. Memasukkan Nilai ke Array"<<"\n";            
+  cout<<"2. Menampilkan Array"<<"\n";            
+  cout<<"3. Menyortir Array"<<"\n";           
+  cout<<"4. info"<<"\n";            
+  cout<<"5. Exit"<<"\n";  
+  cout<<"===================="<<"\n" ;          
+  cout<<"Masukan angka :";        
+}
 
+void inputArray(){
+  system("cls");
+  cout << "Berapa banyak data yang ingin dimasukkan? (max " << MAX << "): ";
+    cin >> jumlah;
+
+    if (jumlah > MAX || jumlah < 1) {
+        cout << "Jumlah tidak valid!";
+        getch();
+        return;
+    }
+
+    for (int i = 0; i < jumlah; i++) {
+        cout << "Masukkan data ke-" << i + 1 << ": ";
+        cin >> data[i];
+    }
+    cout << "Data berhasil disimpan!";
+    getch();
+}
+
+void tampilArray() {
+  system("cls");
+  if (jumlah == 0) {
+      cout << "Array masih kosong!";
+  } else {
+      cout << "Isi Array:\n";
+      for (int i = 0; i < jumlah; i++) {
+          cout << data[i] << " ";
+      }
+  }
+  getch();
+}
+void sortArray() {
+  system("cls");
+  if (jumlah == 0) {
+      cout << "Array masih kosong!";
+  } else {
+      // Bubble Sort
+      for (int i = 0; i < jumlah - 1; i++) {
+          for (int j = 0; j < jumlah - i - 1; j++) {
+              if (data[j] > data[j + 1]) {
+                  int temp = data[j];
+                  data[j] = data[j + 1];
+                  data[j + 1] = temp;
+              }
+          }
+      }
+      cout << "Array berhasil diurutkan!";
+  }
+  getch();
 }
 
 void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
+  system("cls");
+  cout<<"hallo saya menu "<<pesan;
 getch();
 }
 
@@ -36,14 +91,17 @@ do
   switch (pl)
   {
    case '1':
-    /* code */
-    mPertama("pertama");
+   inputArray();
+   mPertama("ke- satu");
+   /* code */
     break;
    case '2':
+   tampilArray();
     mPertama("ke- dua");
     /* code */ 
     break;  
    case '3':
+   sortArray();
     mPertama("ke- tiga");
     /* code */
     break;  
